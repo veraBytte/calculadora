f32af46266b166958d2f4779c11580ebecb9163d
gisellva <depgisell@gmail.com>
2023-09-15 16:27:43 -0500

    logica2

--- calcularoda.js
+++ calcularoda.js
@@ -4,18 +4,21 @@ let matematicas = 0;
 let ingles = 0;
 let geografia = 0;
 
-function agregarInfo(params) {
+function agregarInfo() {
     const materiainput=document.getElementById('materia').value
-    const materias=materiainput
+     materias=materiainput
+    const notasinput=document.getElementById('calificaciones').value
+     notas.push(notasinput)
+     console.log(materias ,notas)
 }
 
-function calcularPromedio(notas, materia) {
+function calcularPromedio() {
 
     if (notas.length > 0) {
         const suma = notas.reduce((total, numero) => total + numero, 0);
         const total=suma / notas.length;
         const promedio = total.toFixed(1)
-        switch (materia) {
+        switch (materias) {
             case "matematicas":
                 matematicas = promedio;
                 break;
@@ -29,11 +32,10 @@ function calcularPromedio(notas, materia) {
                 materias = "";
                 break;
         }
-        materias = materia;
+      
         console.log(promedio, materias);
     } else {
         console.log('no válido');
     }
 }
 
-calcularPromedio([5.0, 3.0, 2.0], "matematicas");
